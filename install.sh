0167eaddfc845d811fab6b61c80bb9c6ffaa39fd
PabloG-Dev <pabloguti51@proton.me>
2025-02-14 22:54:55 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Simple installation script for runitctl
-install_path="/bin/runitctl"
+install_path="/bin"
 
 # Copy the script to /bin and make it executable
-cp runitctl "$install_path"
-chmod +x "$install_path"
+cp runitctl "$install_path/"
+chmod +x "$install_path/runitctl"
 
 echo "runitctl installed successfully! You can now use it system-wide."
+
